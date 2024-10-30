dbaa9b38a7450dd8083eb133c7672f9cfa1f94fc
FelixG105 <felix.gonzalez105@gmail.com>
2024-10-30 13:16:00 -0400

    Stage 3 JS card prep

--- scripts/index.js
+++ scripts/index.js
@@ -36,13 +36,62 @@ const initialCards = [
 
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileSubtitleInput = document.querySelector("#profile-subtitle-input");
+const profileEditForm = document.querySelector(".modal__form");
+
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+/* --------------------------------------------*/
+/* ------------------Functions-----------------*/
+/* --------------------------------------------*/
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+/* --------------------------------------------*/
+/* ------------------Event Handlers------------*/
+/* --------------------------------------------*/
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileSubtitle.textContent = profileSubtitleInput.value;
+  closePopup();
+}
+
+/* --------------------------------------------*/
+/* ------------------Event Listeners-----------*/
+/* --------------------------------------------*/
 
 profileEditBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileSubtitleInput.value = profileSubtitle.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
 const profileModalCloseBtn = document.querySelector("#modal-close-button");
 
-profileModalCloseBtn.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+profileModalCloseBtn.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  //clone the template element with all its content and store it in a cardElement variable
+  const cardElement = cardTemplate.cloneNode(ture);
+  //access the card title and image and store them in variables
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  //set the path to the image to the link field of the object
+  //set the image alt text to the name field of the object
+  //set the card title to the name field of the object, too
+  cardTitleEl.textContent = cardData.name;
+  //return cardElement;
+  //return the ready HTML element with the filled-in data
+  cardListEl.append(cardElement);
 });
