89ce26cacc64e58fbcdea1f12c9e0472307e54fa
FelixG105 <felix.gonzalez105@gmail.com>
2025-02-06 12:52:57 -0500

    updated directories.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Card from "./card.js";
-import FormValidator from "./FormValidator.js";
+import Card from "../scripts/card.js";
+import FormValidator from "../scripts/FormValidator.js";
 
 const initialCards = [
   {
